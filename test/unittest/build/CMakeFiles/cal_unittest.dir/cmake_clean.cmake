7608ae7578e1ac92a1a0621dc8d490a329886d29
danteking <danteking@aliyun.com>
2023-03-24 16:41:24 +0800

    test

---
+++ test/unittest/build/CMakeFiles/cal_unittest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cal_unittest.dir/src/CalTest.cpp.o"
+  "CMakeFiles/cal_unittest.dir/src/main.cpp.o"
+  "cal_unittest.pdb"
+  "cal_unittest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cal_unittest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
